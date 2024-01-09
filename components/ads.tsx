3a3e3928820959aa79485631295db7b7eb6a33fd
Rdx11 <imam.ikhlasul@gmail.com>
2024-01-09 16:40:06 +0700

    adding adsense

---
+++ components/ads.tsx
@@ -0,0 +1,7 @@
+import Script from 'next/script';
+
+const AdScript = () => (
+  <Script src="\/\/crookedchange.com\/bpXhVCsjd.Gtlj0QY\/WkdOisYhW\/5juaZUXeIF\/Xepmq9OuCZfUGlXkAPwTcQR4GM\/zJU\/1_NYTfEXteNTDggCzcN_TWUv1\/MzgI"/>
+);
+
+export default AdScript;
